706b28952d5d92898aef64f66da10bf07461fa40
ehermes <ehermes@wisc.edu>
2015-09-25 10:31:24 -0500

    Still not working, but the files are here

---
+++ ased3/setup.py
@@ -0,0 +1,19 @@
+
+def configuration(parent_package='', top_path=None):
+
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('ased3', parent_package, top_path)
+
+    config.add_library('d3_fort',
+            sources=['d3params.f90', 'd3_fort.f90'])
+
+    config.add_extension('d3_fort',
+            sources=['d3_fort.pyf'],
+            libraries=['_d3_fort'])
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from numpy.distutils.core import Extension
 
-d3fort = Extension(name = 'd3_fort',
-                   sources = ['d3_fort.pyf', 'd3_fort.f90'])
+def configuration(parent_package='', top_path=None):
+
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+
+    config.add_subpackage('ased3')
+
+    return config
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
+
     setup(name = 'ased3',
           description = 'Grimme D3 dispersion calculator for ASE',
           author = 'Eric Hermes',
           author_email = 'ehermes@wisc.edu',
-          ext_modules = [d3fort])
+          configuration=configuration)
